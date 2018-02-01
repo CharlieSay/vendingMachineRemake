45de279ee9e4c98f22d569615d555b0d4222fcb9
Charlie Say <charlie.say@autotrader.co.uk>
2018-02-01 11:12:13 +0000

    Structural Integrity.

--- test/uk/charliesay/VendingMachine/Business/VendingMachineTest.java
+++ test/uk/charliesay/VendingMachine/Business/VendingMachineTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class VendingMachineTest {
 
-    VendingMachine vendingMachine;
+    private VendingMachine vendingMachine;
 
     @Before
     public void setUp(){
